111838bbbad46e179708d2e9d1f79b1e726df561
ericgpinto <ericgrandopinto@gmail.com>
2025-04-13 16:27:32 -0300

    initial commit

---
+++ src/test/java/com/ericpinto/desafiovotacaofullstack/DesafioVotacaoFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericpinto.desafiovotacaofullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioVotacaoFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
