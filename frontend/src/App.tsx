e43d4d6a2292f9c7e96f196051a26de039a40666
ericgpinto <ericgrandopinto@gmail.com>
2025-04-14 16:13:24 -0300

    refactor(project): organiza pastas do projeto

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600">
+      <h1 className="text-white text-4xl font-bold">Hello Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App;
