c984163af4139b2d430445f4d0dab80611c88c4f
ericgpinto <ericgrandopinto@gmail.com>
2025-04-14 15:00:02 -0300

    feat(database): database connection

---
+++ src/main/java/com/ericpinto/desafiovotacaofullstack/domain/vote/exception/EnumConstantException.java
@@ -0,0 +1,7 @@
+package com.ericpinto.desafiovotacaofullstack.domain.vote.exception;
+
+public class EnumConstantException extends RuntimeException {
+    public EnumConstantException(String message) {
+        super(message);
+    }
+}
