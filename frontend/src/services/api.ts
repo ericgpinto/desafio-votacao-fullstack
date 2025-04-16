0edbc5ddd1cdee247197ba78ec4bd60b1f1b31eb
ericgpinto <ericgrandopinto@gmail.com>
2025-04-16 19:29:45 -0300

    fix: url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://desafio-votacao-backend.onrender.com/api/v1",
 });
 
 export default api;
