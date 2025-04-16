060f55d8ef38551e2446a1359cd659be4015be4d
ericgpinto <ericgrandopinto@gmail.com>
2025-04-16 19:46:33 -0300

    fix: cors

--- backend/src/main/java/com/ericpinto/desafiovotacaofullstack/infrastructure/config/CorsConfig.java
+++ backend/src/main/java/com/ericpinto/desafiovotacaofullstack/infrastructure/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://desafio-votacao-frontend.vercel.app")
+                        .allowedOrigins("http://localhost:5173", "https://desafio-votacao-fullstack-frontend.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
