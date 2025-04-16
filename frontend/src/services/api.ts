1bbc204da94dba62a84b52ec230109103d3ec1ab
ericgpinto <ericgrandopinto@gmail.com>
2025-04-16 19:54:43 -0300

    refactor: url local

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://desafio-votacao-backend.onrender.com/api/v1",
+  baseURL: "http://localhost:8080/api/v1",
 });
 
 export default api;
